0f1f01bbf9858eab03f2bf194bf0a6d1dcc548b0
Hossein <ho3einzafari@gmail.com>
2020-07-23 23:08:14 +0430

    Add job column to database and refactor

--- model/user.php
+++ model/user.php
@@ -3,7 +3,7 @@ require_once '../core/loader.php';
 
 class UserModel {
 
-    public static function signUp($name , $family , $phoneNumber , $password): array{
+    public static function signUp($name , $family , $phoneNumber , $job , $password): array{
         global $connection;
         $res = $connection->query("select name from tb_user where phoneNumber='$phoneNumber'");
         if($res->rowCount() > 0){
@@ -13,17 +13,18 @@ class UserModel {
             exit();
         }
         try {
-        global $connection;
-        $command = $connection->prepare("INSERT INTO tb_user (name , family , phoneNumber , password) VALUES (:name , :family , :phoneNumber , :password)");
-        $command->bindParam(":name" , $name);
-        $command->bindParam(":family" , $family);
-        $command->bindParam(":phoneNumber" , $phoneNumber);
-        $command->bindParam(":password" , $password);
-        $command->execute();
-     
-        return responseStd(
-            200 , 'success', "you successfully singup"
-        );
+            global $connection;
+            $command = $connection->prepare("INSERT INTO tb_user (name , family , phoneNumber , job , password) VALUES (:name , :family , :phoneNumber , :job , :password)");
+            $command->bindParam(":name" , $name);
+            $command->bindParam(":family" , $family);
+            $command->bindParam(":phoneNumber" , $phoneNumber);
+            $command->bindParam(":job" , $job);
+            $command->bindParam(":password" , $password);
+            $command->execute();
+        
+            return responseStd(
+                200 , 'success', "you successfully singup"
+            );
     } catch(PDOException $e){
             return responseStd(
                 500 , 'Failed Request', "$e->getMessage()"
--- v1/signup.php
+++ v1/signup.php
@@ -1,7 +1,7 @@
 <?php 
 require_once '../core/loader.php';
 
-if(!isset($_POST['name']) || !isset($_POST['family']) ||!isset($_POST['phoneNumber']) || !isset($_POST['password'])){
+if(!isset($_POST['name']) || !isset($_POST['family']) || !isset($_POST['phoneNumber']) || !isset($_POST['job']) || !isset($_POST['password'])){
     setJsonOutPut();
     echo json_encode(responseStd(400 , 'failed' , 'params is empty or no isset'));
     exit();
@@ -10,10 +10,12 @@ if(!isset($_POST['name']) || !isset($_POST['family']) ||!isset($_POST['phoneNumb
 $name = $_POST['name'];
 $family = $_POST['family'];
 $phoneNumber = $_POST['phoneNumber'];
+$job = $_POST['job'];
 $password = $_POST['password'];
 $hashedPassword = setEncryptPass($password);
 
-$response = UserModel::signup($name , $family , $phoneNumber , $hashedPassword);
+$response = UserModel::signup($name , $family , $phoneNumber , $job , $hashedPassword);
 
+setJsonOutPut();
 echo json_encode($response);
 exit();
